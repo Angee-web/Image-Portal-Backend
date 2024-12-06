b720e19e1fdda10618b7238996ee60ea334acbcf
Adaeze Ugwumba <144529143+Angee-web@users.noreply.github.com>
2024-12-06 22:17:53 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGODB_CONNECTION_STRING;
-        if (!uri) {
-            throw new Error('MongoDB connection string is not defined');
-        }
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected successfully!');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    });
+    console.log("MongoDB connected successfully!");
+    return connection.connection; // Return the connection object
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
