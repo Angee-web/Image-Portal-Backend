39634cea7d01baed4d1e3eb41449b3dd9548aa81
Adaeze Ugwumba <144529143+Angee-web@users.noreply.github.com>
2024-12-06 21:56:04 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 dotenv.config();
 
 // connect to the atlas database with the connection string 8
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+mongoose.connect(process.env.MONGODB_URI);
 
 // assign the connection string to db 9
 const db = mongoose.connection;
